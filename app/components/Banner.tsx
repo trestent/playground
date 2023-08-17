a735345333b36a9132c63f2aee61b930e2a4fb36
Tristan Tenges <tristan.tenges@gmail.com>
2023-08-17 16:41:02 -0500

    add starting pages and files

---
+++ app/components/Banner.tsx
@@ -0,0 +1,12 @@
+import BannerItem from "./BannerItem"
+
+export default function Banner(){
+    return (
+        <div>
+            <BannerItem />
+            <BannerItem />
+            <BannerItem />
+            <BannerItem />
+        </div>
+    )
+}
---
+++ app/components/BannerItem.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+export default function BannerItem(){
+    return (
+        <div>
+            
+        </div>
+    )
+}
